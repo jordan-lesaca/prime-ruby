0bf63a98d0e30a40e7694f09819902ed0247b33b
Jordan Lesaca <jordan.m.lesaca@gmail.com>
2021-03-03 20:50:33 -0800

    commit tree d999e59b9abd0abf4d13cb6e04b6ff0686c3d476

--- prime.rb
+++ prime.rb
@@ -1,1 +1,12 @@
-# Add  code here!
+def prime?(num)
+    for n in 2..(num-1)
+        if num % n == 0 
+            return false
+        end
+    end
+
+    if num < 2
+        return false
+    end
+    return true
+end
